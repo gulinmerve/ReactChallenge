7cdad1b3574effa613359f11b2aff0a803f2fa1d
G.Merve Ayaz <gulineyel@gmail.com>
2023-09-26 12:30:50 +0200

    JSX inline styling added

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,29 +4,12 @@ import './index.css';
 import App from './App';
 
 
-ReactDOM.render(
-  <div>
-    <h1>My Favorite Foods</h1>
-    <ul>
-      <li> Bacon</li>
-      <li> Pizza </li>
-      <li> Hamburger</li>
-    </ul>
-  </div>,
-  document.getElementById("root")
-);
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<App />)
 
 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-    <App />
-//   </React.StrictMode>
-// );
-// reportWebVitals();
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 
 
