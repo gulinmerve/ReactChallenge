26b591334abcfbf5f3419501dfba92c424349d8f
G.Merve Ayaz <gulineyel@gmail.com>
2023-09-27 08:11:35 +0200

    CurrentTime added

---
+++ my-app/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Heading from "./Heading";
+// import List from "./List";
+// import '../styles/App.css';
+
+function App() {
+  return (
+    <div>
+      <Heading />
+    </div>
+  );
+}
+
+export default App;
+
