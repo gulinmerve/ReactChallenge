724abf8cadf394c9a8ffa04b8b8ab4d75aa6a924
G.Merve Ayaz <gulineyel@gmail.com>
2023-10-19 08:34:24 +0200

    time added

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -1,14 +1,22 @@
-import React from "react";
-import Form from "./Form";
-
-var userIsRegistered = false;
+import React, { useState } from "react";
 
 function App() {
+
+  setInterval(updateTime);
+
+  const now = new Date().toLocaleTimeString();
+
+  const [time, setTime] = useState(now);
+
+  function updateTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+
   return (
     <div className="container">
-      <Form 
-      isRegistered = {userIsRegistered}
-      />
+      <h1>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
   );
 }
